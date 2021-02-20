c96a7c36d71bcc872930882799bf11dbf6c16bd5
Vladyslav Koval <49593840+Vldkvll@users.noreply.github.com>
2021-02-20 21:49:15 +0200

    added material

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from "App";
 import { Provider } from "react-redux";
 import store from "redux/store";
 
 ReactDom.render(
     <Provider store={store}>
+        <CssBaseline />
         <App />
     </Provider>,
     document.getElementById("root")
